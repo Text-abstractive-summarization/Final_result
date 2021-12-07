ddc7d8cfae1b28517e161f5a9dd758c62799aa0e
zena-H <63337285+zena-H@users.noreply.github.com>
2021-12-07 19:20:25 +0900

    Update generate.py
    generate update

--- Crawling-main/generate.py
+++ Crawling-main/generate.py
@@ -1,15 +1,28 @@
 from transformers import T5ForConditionalGeneration, T5Tokenizer
+import torch
 
 
 class Generate:
     def __init__(self, model_url):
-        self.model = T5ForConditionalGeneration.from_pretrained(model_url)
+        self.model = torch.load(model_url+'/model.pt')
         self.tokenizer = T5Tokenizer.from_pretrained(model_url)
+        self.device = device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model.to(device)
 
     def generate_text(self, text):
         text = 'summarize :'+text
-        ids = self.tokenizer.encode(text, return_tensors='pt')
-        generate = self.model.generate(ids)
+        data = self.tokenizer.encode_plus(text, return_tensors='pt')
+        ids = data['input_ids'].to(self.device,dtype=torch.long)
+        mask = data['attention_mask'].to(self.device,dtype=torch.long)
+        
+        generate = self.model.generate(input_ids = ids,
+                attention_mask = mask, 
+                max_length=150, 
+                num_beams=2,
+                repetition_penalty=2.5, 
+                length_penalty=1.0, 
+                early_stopping=True)
+
         return self.tokenizer.decode(generate[0],skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
     def input_generate(self, df, column):
